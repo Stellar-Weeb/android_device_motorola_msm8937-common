7ee5750eedb59caefa8b4ac460f4a7beba8c8942
nift4 <nift4@protonmail.com>
2021-08-08 15:09:16 +0200

    msm8937-common: init.extra: set optimized swapiness & lmk config
    Change-Id: Ib25084c92ce054357414cb9232eb27087d91078f

--- rootdir/bin/init.extra.sh
+++ rootdir/bin/init.extra.sh
@@ -1,1 +1,5 @@
 ### extras ###
+# Override CAF's lowmemorykiller values with better ones for both 2 and 3GB RAM models
+echo 14130,18840,23550,51810,60650,69160 > /sys/module/lowmemorykiller/parameters/minfree
+# Set swapiness to 150. We only use super-cheap (of CPU time) ZRAM.
+echo 150 > /proc/sys/vm/swappiness
